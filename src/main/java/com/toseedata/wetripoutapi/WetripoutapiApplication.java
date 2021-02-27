79f1f79f3e0b0d2b177973f652b06474efb6b141
Dave Callaghan <david.callaghan@perficient.com>
2021-02-27 14:03:14 -0500

    initial

---
+++ src/main/java/com/toseedata/wetripoutapi/WetripoutapiApplication.java
@@ -0,0 +1,13 @@
+package com.toseedata.wetripoutapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetripoutapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetripoutapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toseedata/wetripoutapi/WetripoutapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toseedata.wetripoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetripoutapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
